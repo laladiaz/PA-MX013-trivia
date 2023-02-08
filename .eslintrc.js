c6227e787f4d4ac7c5f4246fa82891675cdca286
laladiaz <mariaabelardadiaz@gmail.com>
2023-02-08 16:37:50 -0600

    adds rules to eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,5 +11,26 @@ module.exports = {
         "sourceType": "module"
     },
     "rules": {
+        "linebreak-style": 0,
+    "prefer-destructuring": 0,
+    "import/extensions": 0,
+    "import/prefer-default-export": 0,
+    "no-trailing-spaces": "error",
+    "no-const-assign": 1,
+    "no-extra-semi": 0,
+    "semi": 0,
+    "no-fallthrough": 0,
+    "no-empty": 0,
+    "no-mixed-spaces-and-tabs": 0,
+    "no-redeclare": 0,
+    "no-this-before-super": 1,
+    "no-undef": 1,
+    "no-unreachable": 1,
+    "no-use-before-define": 0,
+    "constructor-super": 1,
+    "curly": 2,
+    "eqeqeq": 1,
+    "func-names": 0,
+    "valid-typeof": 1,
     }
 }
